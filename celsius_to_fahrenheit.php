f3497dfd8d8b104fbc9ef7168238abf2c1097a25
bulbul427 <160498923+bulbul427@users.noreply.github.com>
2024-07-06 18:00:17 +0600

    Add files via upload

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,6 @@
+<?php
+// Celsius to Fahrenheit Conversion (30)
+$celsius = 32;
+$fahrenheit = (($celsius*9)/5)+32 ;
+printf("The Temperature in Fahrenheit is %.2f", $fahrenheit);
+
---
+++ largest_number.php
@@ -0,0 +1,15 @@
+<?php
+// Find the largest number using PHP (30)
+$num1=4;
+$num2=5;
+$num3=6;
+
+if($num1>$num2 && $num1>$num3){
+    echo "$num1 is the largest number";
+}elseif($num2>$num3 && $num2>$num3){
+    echo "$num2 is the largest number";
+}else{
+    echo "$num3 is the largest number";
+}
+
+
